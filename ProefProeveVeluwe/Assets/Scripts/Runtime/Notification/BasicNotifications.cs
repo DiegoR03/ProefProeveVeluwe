adf04184807fdc935580fa416b053a8d0adf874f
RobinKnol <Robin@knol.email>
2023-02-20 13:26:53 +0100

    finished notification system

--- ProefProeveVeluwe/Assets/Scripts/Runtime/Notification/BasicNotifications.cs
+++ ProefProeveVeluwe/Assets/Scripts/Runtime/Notification/BasicNotifications.cs
@@ -3,27 +3,31 @@ using UnityEngine;
 
 public class BasicNotifications : MonoBehaviour
 {
-    private string DistanceNotication = "Distance_Notification";
+    // Creates a channel to allow notifications to be send to the phone notification network
     private void Start()
     {
         var DistanceNotificationChannel = new AndroidNotificationChannel
         {
-            Id = DistanceNotication,
+            Id = "Distance_Notification",
             Name = "Afstand Notficatie",
             Description = "Dit geeft je een notificatie waarneer je in de buurt bent van een spelletje",
             Importance = Importance.High
         };
         
         AndroidNotificationCenter.RegisterNotificationChannel(DistanceNotificationChannel);
+    }
 
+    // Makes a notification on call to send ot the phone notificaiton in the Distance Notification Channel
+    public static void Notification(string title, string text)
+    {
         var ClostToWaypointNotification = new AndroidNotification
         {
-            Title = "Veluwe Picknic",
-            Text = "Je bent nu dichtbij een minigame",
+            Title = title,
+            Text = text,
             SmallIcon = "icon_0",
             LargeIcon = "icon_1",
         };
-
-        AndroidNotificationCenter.SendNotification(ClostToWaypointNotification, DistanceNotication);
+    
+        AndroidNotificationCenter.SendNotification(ClostToWaypointNotification, "Distance_Notification");
     }
 }
