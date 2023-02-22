fead67b6dc8fc5cf31b7ff73acb3922949d35bc3
RobinKnol <Robin@knol.email>
2023-02-22 15:27:06 +0100

    Added a hand when petting

--- ProefProeveVeluwe/Assets/Scripts/Runtime/Petting.cs
+++ ProefProeveVeluwe/Assets/Scripts/Runtime/Petting.cs
@@ -5,6 +5,8 @@ public class Petting : MonoBehaviour
 {
     [SerializeField] private Camera MainCamera;
     [SerializeField] private Slider HappinessBar;
+    [SerializeField] private GameObject PetHand;
+    [SerializeField] private int PetTime;
 
     private Ray _ray;
     private RaycastHit _hit;
@@ -16,12 +18,17 @@ public class Petting : MonoBehaviour
 
     private void Update()
     {
-        if (Input.touchCount <= 0 && Input.touches[0].phase != TouchPhase.Began) return;
+        if (Input.touchCount <= 0)
+        {
+            PetHand.SetActive(false);
+            return;
+        }
+
         _ray = MainCamera.ScreenPointToRay(Input.touches[0].position);
+        // It will hit the background so specify the hit
+        if (!Physics.Raycast(_ray, out _hit) || _hit.collider == null) return;
         
-        if (!Physics.Raycast(_ray, out _hit)) return;
-        if (_hit.collider == null) return;
-        
-        HappinessBar.value += 0.05f;
+        HappinessBar.value += Time.deltaTime/PetTime;
+        PetHand.SetActive(true);
     }
 }
